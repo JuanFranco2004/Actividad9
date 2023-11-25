3cc9947bcc440d48f11562937df70a1ad4ee139c
JuanFranco2004 <jemafm04@gmail.com>
2023-11-24 21:57:52 -0300

    tallerphp16

---
+++ dao/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('SERVER','localhost:3306');
+    define('USER','Juan');
+    define('PASSWORD', 'Morad@123');
+    define('DATABASE', 'base23');
+    define('CHARSET', 'utf-8');
+?>
